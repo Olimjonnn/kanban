eea6ae9a5e71767e0af5291c56a01d5ca4982274
Olimjon <olimjonolimjov3@gmail.com>
2024-05-20 16:47:44 +0500

    some changes

--- user/admin.py
+++ user/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-# from user.models import User
+from user.models import User
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext, gettext_lazy as _
 
 
-# @admin.register(User)
-# class UserAdmin(UserAdmin):
-#     list_display = ["id", "username", 'is_active', 'is_staff', 'is_superuser']
-#     fieldsets = (
-#         (None, {'fields': ('username', 'password')}),
-#         (_('Permissions'), {
-#             'fields': ('is_active', 'is_staff', "is_superuser", 'user_permissions'),
-#         }),
-#         (_('Important dates'), {"fields": ('created_date',)})
-#     )
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ["id", "phone", 'is_active', 'is_staff', 'is_superuser']
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', "is_superuser", 'user_permissions'),
+        }),
+        (_('Important dates'), {"fields": ('created_date',)})
+    )
